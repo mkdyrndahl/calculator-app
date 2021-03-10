0dfc6ba79682b0083fd2b4fae7b5fbf017ed2052
Matt Dyrndahl <mkdyrndahl@gmail.com>
2021-03-09 16:43:28 -0800

    Added addition as an option

--- src/Components/CalculatorForm/CalculatorFormMDy87.js
+++ src/Components/CalculatorForm/CalculatorFormMDy87.js
@@ -11,14 +11,14 @@ function CalculatorFormMDy87(props){
             <div className="col-4">
                 <select className="form-select" id="operation" onChange={props.update}>
                     <option selected>Please select an operation</option>
-                    
+                    <option value="add">Addition</option>
                 </select>
             </div>
             <div className="col-4">
                 <label for="operand2" className="col2 col-form-label">Operand 2</label>
                 <input type="text" className="form-control-plaintext" id="operand2" onChange={props.update}/>
             </div>
-            <button onClick={props.action}/>
+            <button onClick={props.action}>Execute!</button>
         </div>
 
     )
