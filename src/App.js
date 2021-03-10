e696c3cf4c4e8d102733558275cb61fd1dcf241c
Matt Dyrndahl <mkdyrndahl@gmail.com>
2021-03-09 16:29:12 -0800

    Added Skeleton Interface to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react"
+import CalculatorFormMDy87 from "./Components/CalculatorForm/CalculatorFormMDy87"
 
 function App() {
   return (
     <div className="App">
       <h1>This is a calculator</h1>
-      
+      <CalculatorFormMDy87></CalculatorFormMDy87>
 
     </div>
   );
---
+++ src/Components/CalculatorForm/CalculatorFormMDy87.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+function CalculatorFormMDy87(props){
+
+    return (
+        <div className="row">
+            <div className="col-4">
+                <label for="operand2" className="col2 col-form-label">Operand 1</label>
+                <input type="text" className="form-control-plaintext" id="operand1" onChange={props.update}/>
+            </div>
+            <div className="col-4">
+                <select className="form-select" id="operation" onChange={props.update}>
+                    <option selected>Please select an operation</option>
+                    
+                </select>
+            </div>
+            <div className="col-4">
+                <label for="operand2" className="col2 col-form-label">Operand 2</label>
+                <input type="text" className="form-control-plaintext" id="operand2" onChange={props.update}/>
+            </div>
+            <button onClick={props.action}/>
+        </div>
+
+    )
+}
+
+export default CalculatorFormMDy87;
