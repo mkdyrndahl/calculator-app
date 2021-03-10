5a421195d79a72254e6cbd949eee13b3cc99c702
Matt Dyrndahl <mkdyrndahl@gmail.com>
2021-03-09 16:46:40 -0800

    Attempted to fix issues with addition function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react"
+import React, { useState } from "react"
 import CalculatorFormMDy87 from "./Components/CalculatorForm/CalculatorFormMDy87"
 
 function App() {
+
+  const [equation, setEquation] = useState({});
+  
+  function updateEquation(e) {
+    setEquation({
+      ...equation,
+      [e.target.id] : [e.target.value]
+    })
+  }
+
+  function doOperation(e){
+    switch(equation.operation){
+      case "add":
+        // do add
+        console.log("Result = " + (parseFloat(equation.operand1) + parseFloat(equation.operand2)))
+        break;
+      default:
+        console.log("no`")
+      break;
+    }
+  }
+
   return (
     <div className="App">
       <h1>This is a calculator</h1>
-      <CalculatorFormMDy87></CalculatorFormMDy87>
+      <CalculatorFormMDy87 update={updateEquation} action={doOperation}/>
 
     </div>
   );
